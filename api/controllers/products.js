e66d6ae4c66f3d8a08bb1a534f183cae0356904b
alk831 <adam54111@gmail.com>
2018-09-19 16:50:22 +0200

    create product routes & handle db logic

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -3,19 +3,45 @@ const productSchema = require('../schemas/product');
 const Product = require('../models/product');
 
 exports.get_product = async (req, res, next) => {
-  return { msg: 'This is GET /product route !' };
+  const { productId } = req.params;
+
+  const product = await Product.findOne({ _id: productId });
+
+  return {
+    product
+  };
 };
 
 exports.post_product = async (req, res, next) => {
   const { product } = req.body;
   await Joi.validate(product, productSchema);
-  
-  const newProduct = new Product(product);
 
-  const addedProduct = await newProduct.save();
+  const { _id: id } = await new Product(product).save();
 
   return {
-    status: 202,
-    addedProduct
+    status: 201,
+    msg: `Product has been successfully created with id ${id}`,
+    id
+  };
+};
+
+exports.patch_product = async (req, res, next) => {
+  const { productId } = req.params;
+  const { product } = req.body;
+
+  await Product.where({ _id: productId }).updateOne(product);
+
+  return {
+    msg: `Product with id ${productId} has been successfully updated`
+  };
+};
+
+exports.delete_product = async (req, res, next) => {
+  const { productId } = req.params;
+
+  await Product.deleteOne({ _id: productId });
+
+  return {
+    msg: `Product with id ${productId} has been successfully deleted`
   };
 };
--- api/routes/products.js
+++ api/routes/products.js
@@ -7,4 +7,8 @@ router.get('/:productId', handler(productsController.get_product));
 
 router.post('/', handler(productsController.post_product));
 
+router.patch('/:productId', handler(productsController.patch_product));
+
+router.delete('/:productId', handler(productsController.delete_product));
+
 module.exports = router;
